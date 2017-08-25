070d895e5e7e2b616f55e5f438ce75f8541d2775
amalmazar <amalmazar@gmail.com>
2017-08-24 21:53:11 -0400

    adding hangman game

--- Week03/Hangman-Game/assets/javascript/game.js
+++ Week03/Hangman-Game/assets/javascript/game.js
@@ -17,7 +17,7 @@ X computer selects a random word from an array of words
     // X. Subtract guesses from initialGuesses
     // X. Replace _'s with correctly guessed letters
     // 4. Turn random word into an array of letters
-    // 5. 
+    // 5. put all guesses into an array, READ ARRAY TO PLACE INCORRECTLY GUESSED LETTERS INSTEAD OF DOING THE OTHER THING!
     
 // > NICE TO HAVES
     // Hit enter to submit guess
@@ -27,36 +27,37 @@ X computer selects a random word from an array of words
 // Array of words
     var words = ["boston", "shiba", "cat"];
 
-// Number of guesses
+    // Blank array of number of guesses
     var initialGuesses = 10;
     var guessedLetters = [];
         
 // This function is run whenever the user presses a key.
-    document.onkeyup = function(event) {
-
-    // Determines which key was pressed.
-       var userGuess = document.getElementById("guess").value;
-            console.log(userGuess);
-
-
-    // Adds user guess to the incorrect list
-        function guessHandle() {
-            //*Correct guess to-dos
-
-            // Incorrect guess to-dos
-                document.getElementById("incorrect").innerHTML += userGuess;
-                initialGuesses = initialGuesses - 1;
-                document.getElementById("Guesses-left").innerHTML = initialGuesses;
+    document.getElementById("guess-letter-box").onkeydown = function(event) {
+        // Check if enter is pressed
+        if (event.keyCode == 13) {
+            // Determines which key was pressed.
+            var userInput = document.getElementById("guess-letter-box").value;
+            // Add letter to user input array
+            guessedLetters.push(userInput);
+            // Display guessed letters
+            document.getElementById("incorrect-guesses-box").innerHTML = guessedLetters;        
+            // Subtract one from initial guesses
+            initialGuesses = initialGuesses - 1;
+            // Display new initial guess count
+            document.getElementById("Guesses-left").innerHTML = initialGuesses;
+            // Shows guessed letter array in console
+            console.log(guessedLetters);
+            // Compare user input letter to random word *passing it through a hole in the wall*
+            showCorrectLetters(userInput)
         }
-        guessHandle()
-        guessedLetters.push(userGuess);
-        console.log(initialGuesses);
     }
+    
     
 // Computer selects a random word from an array of words
     var randomWord = words[Math.floor(Math.random() * words.length)];
 
-// Computer counts letters from the random words and logs it to the console.
+/*
+// FIGURE OUT WHAT THIS DOES!!
     function randomWordLength(str) {
         var letters = 0;
         var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -68,22 +69,45 @@ X computer selects a random word from an array of words
         }
         return letters;
     }
+*/
 
+// Computer counts letters from the random words and logs it to the console.
 var randomWordLetters = randomWord.split("");
 console.log(randomWordLetters)
 
-//Computer displays number of letters represented by _.
-    function answerSpots() {
+
+    //Display number of letters represented by _ or correct letters
+    function showCorrectLetters(userInput) {
+        // Run user input letter through the random word letters to see if the match
+        for (i = 0; i < randomWordLetters.length; i++) {
+            // Check if user input letter matches random word letter
+            if(userInput == randomWordLetters[i]) {
+                // Show correct letter
+                underscoresOrLetters += userInput;
+            }
+            else {
+            // Show how many letters are in the random word with underscores
+            underscoresOrLetters += "_ "
+            }
+        // Display underscores representing random word letters
+        document.getElementById("guessing-stage").innerHTML = underscoresOrLetters;
+        }
+    } 
+    // Create underscore as a blank string
+    var underscoresOrLetters="";
+    //showCorrectLetters();
+
+
+    
+/* Copy of function in case we break it.
+//Display number of letters represented by _ or correct letters
+    function showCorrectLetters() {
+        //Loop to count letters in random word
         for (i = 0; i < randomWord.length; i++) {
+            // Show how many letters are in the random word with underscores
             underscore += "_ "
         }
+        // Display underscores representing random word letters
         document.getElementById("guessing-stage").innerHTML = underscore;
-        
-        //Computer shows correct  letters above the _
-        document.getElementById("correct-letters").innerHTML = randomWordLetters.join(" ");
     } 
-
-    var underscore="";
-    answerSpots();
-
-
+*/
