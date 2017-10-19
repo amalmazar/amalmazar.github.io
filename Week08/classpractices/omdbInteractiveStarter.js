105340bbca0b8394f2ce155e9b88d6e795a0d358
amalmazar <amalmazar@gmail.com>
2017-10-19 17:41:37 -0400

    first

---
+++ Week08/classpractices/omdbInteractiveStarter.js
@@ -0,0 +1,41 @@
+// INSTRUCTIONS:
+// ---------------------------------------------------------------------------------------------------------
+// Level 1:
+// Take any movie with a word title (ex: Cinderella) as a Node argument and retrieve the year it was created
+
+// Level 2 (More Challenging):
+// Take a move with multiple words (ex: Forrest Gump) as a Node argument and retrieve the year it was created.
+// ---------------------------------------------------------------------------------------------------------
+
+// Include the request npm package (Don't forget to run "npm install request" in this folder first!)
+var request = require("request");
+
+// Then run a request to the OMDB API with the movie specified
+
+var movieName = process.argv[2];
+
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+
+request(queryUrl, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+        console.log("The movie's release date is: " + JSON.parse(body).Released);
+    }
+});
+
+
+// This line is just to help us debug against the actual URL.
+console.log(queryUrl);
+
+
+// Then create a request to the queryUrl
+// ...
+request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+    
+});
+
+
+  // If the request is successful
+  // ...
+
+  // Then log the Release Year for the movie
+  // ...
