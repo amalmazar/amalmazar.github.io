281f13748720148b7e6912434c65cc54473c7aeb
amalmazar <amalmazar@gmail.com>
2017-10-27 23:13:10 -0400

    homework

---
+++ Week11/NodeHangman/letter.js
@@ -0,0 +1,11 @@
+var letterToGuess = "";
+
+exports.letterObject = function(poo) {
+    letterToGuess = poo;
+};
+
+// tell letter what letter he is
+// this is the letter the user guessed
+// are you that letter?
+// yes? return letter
+// else return _
