a4ddba6f34043fddf9480c5b6f7fc07b6966f24c
amalmazar <amalmazar@gmail.com>
2017-11-18 07:46:00 -0500

    burger

--- Week14/eatdaburger/config/connection.js
+++ Week14/eatdaburger/config/connection.js
@@ -1,17 +1,25 @@
-var connection = mysql.createConnection({
+var mysql = require("mysql");
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "",
-  database: "burgers"
-});
+  database: "burgers_db"
+    })
+};
 
 connection.connect(function(err) {
-  if (err) throw err;
+  if (err) {
+  console.error("error connectiong: " + err.stack);
+  return;
+  }
   console.log("connected as id " + connection.threadId + "\n");
-  // run intialize function here?;
 });
+
+module.exports = connection;
